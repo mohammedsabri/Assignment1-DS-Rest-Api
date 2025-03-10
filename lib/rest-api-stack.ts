da6bf801c36e6e3eda8cfc015cd4d9350a9a8d21
mohammed sabri <mohammedsabri228@gmail.com>
2025-03-10 14:29:31 +0000

    Exercise: Delete movie endpoint

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -69,6 +69,18 @@ export class RestAPIStack extends cdk.Stack {
     },
   });
 
+  const newDeleteMovieFn = new lambdanode.NodejsFunction(this, "DeleteMovieFn", {
+    architecture: lambda.Architecture.ARM_64,
+    runtime: lambda.Runtime.NODEJS_22_X,
+    entry: `${__dirname}/../lambdas/deleteMovie.ts`,
+    timeout: cdk.Duration.seconds(10),
+    memorySize: 128,
+    environment: {
+      TABLE_NAME: moviesTable.tableName,
+      REGION: "eu-west-1",
+    },
+  });
+
         
         new custom.AwsCustomResource(this, "moviesddbInitData", {
           onCreate: {
@@ -90,6 +102,7 @@ export class RestAPIStack extends cdk.Stack {
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
         moviesTable.grantReadWriteData(newMovieFn)
+        moviesTable.grantReadWriteData(newDeleteMovieFn)
             // other permissions ....
    
 
@@ -119,6 +132,10 @@ export class RestAPIStack extends cdk.Stack {
     moviesEndpoint.addMethod(
       "POST",
       new apig.LambdaIntegration(newMovieFn, { proxy: true })
+    );
+    moviesEndpoint.addMethod(
+      "DELETE",
+      new apig.LambdaIntegration(newDeleteMovieFn, { proxy: true })
     );
     
     // Detail movie endpoint
